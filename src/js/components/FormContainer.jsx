667ba18a4cb223254ff317cc2fdcd2c02f54e204
Julia Robinson <juliaarobinson@hotmail.com>
2019-06-05 22:36:01 +0100

    Basic form

--- src/js/components/FormContainer.jsx
+++ src/js/components/FormContainer.jsx
@@ -14,13 +14,42 @@ class FormContainer extends Component {
   }
   render() {
     return (
-      <form id="article-form">
-        <input
-          text="SEO"
-          label="seo_title"
-          type="text"
-        />
-      </form>
+      <div className="email-form-wrapper">
+        <form className="email-form">
+          <h1 className="email-form__title">Send E-mail</h1>
+          <fieldset className="email-form__fieldset">
+            <input
+              placeholder="To"
+              type="email"
+              className="email-form__field"
+            />
+            <input
+              placeholder="CC"
+              type="email"
+              className="email-form__field"
+            />
+            <input
+              placeholder="BCC"
+              type="email"
+              className="email-form__field"
+            />
+            <input
+              placeholder="Subject"
+              type="text"
+              className="email-form__field"
+            />
+            <textarea
+              placeholder="Message"
+              rows="10"
+              className="email-form__field email-form__field--textarea"
+            />
+            <input type="file" multiple className="email-form__file-upload" />
+            <button type="submit" className="email-form__button">
+              Send
+            </button>
+          </fieldset>
+        </form>
+      </div>
     );
   }
 }
